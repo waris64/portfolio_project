c17d43fa22f3d849594a8522d788bf392bd73c62
waris64 <warismuhammad652@gmail.com>
2023-11-28 23:34:29 +0500

    Front_Page completed

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -1,14 +1,23 @@
-import "./HeroImg.css";
-import React from 'react'
+import React from 'react';
+import './HeroImg.css';
+import pic from '../assets/pic.jpg';
+import { Link } from 'react-router-dom';
 
 const HeroImg = () => {
   return (
-    <div className="hero">
-        <div className="mask">
-            
+   
+    <div className="hero ">
+      <div className='content' >
+        <p>Hi, I am Freelancer</p>
+        <h1>React Developer</h1>
+        <div>
+          <button className='btn-light'><Link to="Contact" className='btn'>Contact</Link></button>
+          <button className='btn-dark'><Link to="Project" className='btn'>Projects</Link></button>
         </div>
+      </div>
     </div>
-  )
+
+  );
 }
 
-export default HeroImg
+export default HeroImg;
